48ecc611874ad0f9dfd562606ffaa0bce2085f84
Justin Love <git@JustinLove.name>
2018-12-14 11:19:22 -0600

    footer credits with icons

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ My build command:
 `bin/monitor.bat` has a command using the [watch](https://www.npmjs.com/package/watch) CLI
 
 Once built, `public/index.html` can be opened locally from the filesystem, or set up for a local or internet server as you wish.
+
+## Credits
+
+Icons: [IcoMoon - Free](https://icomoon.io/#icons-icomoon) ([CC BY 4.0](http://creativecommons.org/licenses/by/4.0/))
--- src/View.elm
+++ src/View.elm
@@ -4,6 +4,8 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on)
 import Json.Decode
+import Svg exposing (svg, use)
+import Svg.Attributes exposing (xlinkHref)
 
 type Msg
   = SetUsername String
@@ -19,6 +21,24 @@ body {
   background-color: rgb(23, 20, 31);
   color: rgb(218, 216, 222);
 }
+footer {
+  position: fixed;
+  bottom: 0;
+}
+svg.icon {
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.2em;
+  stroke-width: 0;
+  stroke: currentColor;
+  fill: currentColor;
+}
+.icon-github { color: #888; }
+.icon-twitter { color: #55acee; }
+.icon-twitch { color: #6441A4; }
+a:link, a:visited { color: #b19dd8; }
+a:hover, a:active { color: rgb(218, 216, 222); }
 """
 
 document tagger model =
@@ -47,6 +67,7 @@ view model =
             |> List.map displayHost
             |> ul []
           ]
+    , displayFooter
     ]
 
 displayHost : Host -> Html Msg
@@ -71,3 +92,21 @@ targetValue : Json.Decode.Decoder a -> (a -> Msg) -> Json.Decode.Decoder Msg
 targetValue decoder tagger =
   Json.Decode.map tagger
     (Json.Decode.at ["target", "value" ] decoder)
+
+displayFooter : Html msg
+displayFooter =
+  footer []
+    [ a [ href "https://github.com/JustinLove/stream-credits" ]
+      [ icon "github", text "stream-credits" ]
+    , text " "
+    , a [ href "https://twitter.com/wondible" ]
+      [ icon "twitter", text "@wondible" ]
+    , text " "
+    , a [ href "https://twitch.tv/wondible" ]
+      [ icon "twitch", text "wondible" ]
+    ]
+
+icon : String -> Html msg
+icon name =
+  svg [ Svg.Attributes.class ("icon icon-"++name) ]
+    [ use [ xlinkHref ("symbol-defs.svg#icon-"++name) ] [] ]
